1e9a5a9fd70bcaaf69a1e7e7cefbb0c6bce4c166
Lucas Almeida <39913588+lucas-almeida026@users.noreply.github.com>
2023-08-27 21:21:48 -0300

    update

--- lib/id.ts
+++ lib/id.ts
@@ -1,7 +1,7 @@
 function* IdMaker() {
-  let id = 1
-  while (true) {
-    yield id++
-  }
+	let id = 1;
+	while (true) {
+		yield id++;
+	}
 }
-export const idMaker = IdMaker()
+export const idMaker = IdMaker();
--- lib/memory.ts
+++ lib/memory.ts
@@ -1,31 +1,31 @@
 export interface ObjectPoolT<T extends object> {
-  get(): T;
-  return(o: T): void;
+	get(): T;
+	return(o: T): void;
 }
 
 export function ObjectPool<T extends object>(
-  initialSize: number,
-  maxSize: number,
-  factory: () => T,
-  reset: (o: T)=> void
+	initialSize: number,
+	maxSize: number,
+	factory: () => T,
+	reset: (o: T) => void,
 ): ObjectPoolT<T> {
-  const pool: T[] = Array.from({ length: initialSize }, factory);
+	const pool: T[] = Array.from({ length: initialSize }, factory);
 
-  return {
-    get(){
-      if (pool.length > 0) {
-        return pool.pop()!;
-      }
+	return {
+		get() {
+			if (pool.length > 0) {
+				return pool.pop()!;
+			}
 
-      if (pool.length < maxSize) {
-        return factory();
-      }
+			if (pool.length < maxSize) {
+				return factory();
+			}
 
-      throw new Error('Out of memory');
-    },
-    return(o: T){
-      reset(o);
-      pool.push(o);
-    }
-  }
+			throw new Error('Out of memory');
+		},
+		return(o: T) {
+			reset(o);
+			pool.push(o);
+		},
+	};
 }
